f95a6d6a623600324a4f4a309b2d341bdf42eb85
C1217635 Carlos Eduardo Dionizio <root@esbsa9905eddy>
2016-04-12 15:19:34 -0300

    Final do Script somaInteiro

--- src/proj_matematico/ExercicioJava.java
+++ src/proj_matematico/ExercicioJava.java
@@ -1,47 +1,34 @@
+
 package proj_matematico;
 
 public class ExercicioJava {
-	
+
 	int somaInteiro;
-	int limiteInicio = 0;
-	int limiteFim = 50;
-	int total;
-	
+	int limiteInicio;
+	int limiteFim;
+
+	public int getSomaInteiro() {
+		return somaInteiro;
+	}
 
 	public void setSomaInteiro(int somaInteiro) {
 		this.somaInteiro = somaInteiro;
 	}
 
-
 	public int getLimiteInicio() {
 		return limiteInicio;
 	}
-
 
 	public void setLimiteInicio(int limiteInicio) {
 		this.limiteInicio = limiteInicio;
 	}
 
-
 	public int getLimiteFim() {
 		return limiteFim;
 	}
 
-
 	public void setLimiteFim(int limiteFim) {
 		this.limiteFim = limiteFim;
 	}
-	
 
-public void somaInteiro(int limiteInicio, int limiteFim) 
-	
-	{
-	
-		setLimiteInicio(limiteInicio);
-		setLimiteFim(limiteFim);
-		
-	}
-	
-	
 }
-
--- src/proj_matematico/main.java
+++ src/proj_matematico/main.java
@@ -1,28 +1,44 @@
+
 package proj_matematico;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-				
-			
-			ExercicioJava somaInteiro = new ExercicioJava();
 		
-			while (somaInteiro.limiteInicio <= somaInteiro.limiteFim) {
-				
-				int total = somaInteiro.limiteInicio+(somaInteiro.limiteInicio++);
-				
-			System.out.printf("\nA soma dos valores é : " + total );  
-				
-								
-				
-			}
+		//Inicio do script somaInteiro
+
+		Scanner input = new Scanner(System.in);
+
+		ExercicioJava somaInteiro = new ExercicioJava();
+		
+//		Pede ao usuário os dois intervalos da PA
+//		a1 = primeiro termo = 1 (neste caso)
+//		an = enésimo termo = 100 (o último termo, neste caso)
+//		n = quantidade de números da PA
+//		S = soma dos termos da PA
+//
+//		S = (a1 + an).n/2 
+		
+
+		System.out.println("Informe o primeiro termo : ");
+		somaInteiro.limiteInicio = input.nextInt();
+
+		System.out.println("Informe o segundo termo :");
+		somaInteiro.limiteFim = input.nextInt();
+		
+		// Realiza a operação de calculo de soma da PA (
+
+		int total = (somaInteiro.limiteInicio + somaInteiro.limiteFim) * somaInteiro.limiteFim / 2;
+
+		System.out.printf("\nA soma dos valores é : " + total);
+
+		// Fim do Script somaInteiro
 	}
 	
+
 }
-
-
-
-		
